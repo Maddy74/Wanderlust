188715602193a8f4f6edbc0a8cdcd89e9e892035
Maddy74 <madhavsharmag80@gmail.com>
2024-12-28 12:31:30 +0530

    Fixed ExpressError Not defined

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-
+const ExpressError = require("../utils/ExpressError.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
